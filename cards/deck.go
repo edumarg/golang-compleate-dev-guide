52764018c85a8e7c48f002a814fbf837737b25d0
Eduardo Margulis <edumarg@gmx.com>
2022-06-21 09:53:36 +0300

    newDeck function

--- cards/deck.go
+++ cards/deck.go
@@ -10,8 +10,20 @@ type deck []string
 // receiver function: a function that receives a variable.
 // func (<variable name> <variable type>) <function name> {logic}
 
+func newDeck() deck {
+	cards := deck{}
+	cardsSuits := []string{"Clubs", "Spades", "Diamonds", "Harts"}
+	cardsValues := []string{"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"}
+	for _, suit := range cardsSuits {
+		for _, value := range cardsValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+	return cards
+}
+
 func (d deck) print() { // receiver function that receives any variable type deck and use it inside the function logic
-	for i, card := range d {
-		fmt.Println(i, card)
+	for _, card := range d {
+		fmt.Println(card)
 	}
 }
--- cards/main.go
+++ cards/main.go
@@ -1,7 +1,6 @@
 package main
 
 func main() {
-	cards := deck{"Five of Clubs", "King of Spades", "Queen of Hearts"}
-	cards = append(cards, "Ten of Diamonds")
+	cards := newDeck()
 	cards.print()
 }
