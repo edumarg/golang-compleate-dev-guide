b34c716ecf1de7dbde746a0f6bf5d40b6cee40d9
Eduardo Margulis <edumarg@gmx.com>
2022-06-22 11:03:31 +0300

    itarare over map

--- maps/main.go
+++ maps/main.go
@@ -13,6 +13,36 @@ func main() {
 		"red":   "#FF0000",
 		"green": "00FF00",
 		"blue":  "#0000FF",
+		"white": "#ffffff",
 	}
+
 	fmt.Println(colors)
+
+	// other way to declare maps
+	var secondaryColors map[string]string = map[string]string{}
+	fmt.Println(secondaryColors)
+	secondaryColors["darkRed"] = "a60000"
+	secondaryColors["darkBlue"] = "#004080"
+	secondaryColors["darkGreen"] = "#008000"
+	fmt.Println(secondaryColors)
+	delete(secondaryColors, "darkGreen") // this is the same for all ways of declaring map
+	fmt.Println(secondaryColors)
+
+	// another way to declare maps
+	primaryColors := make(map[string]string)
+	fmt.Println(primaryColors)
+	primaryColors["Yellow"] = "ffff00"
+	primaryColors["Red"] = "ff0000"
+	primaryColors["Blue"] = "0000ff"
+	fmt.Println(primaryColors)
+
+	printColors(colors)
+
+}
+
+// iterate over a color map
+func printColors(c map[string]string) {
+	for color, hex := range c {
+		fmt.Println(color, hex)
+	}
 }
