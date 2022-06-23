e9a807c2789bdce70547144ff00965a097e6ee1e
Eduardo Margulis <edumarg@gmx.com>
2022-06-23 10:30:09 +0300

    extended example by adding print version function

--- interfaces/main.go
+++ interfaces/main.go
@@ -9,15 +9,22 @@ import (
 // different values but the logic of the function is similar or has the same objective.
 type templateBot interface { // interface name
 	getGreeting(string) string //<function name>(list of arguments types) list of return types
+	getVersion() string
 }
-type englishBot struct{}
-type spanishBot struct{}
+type englishBot struct {
+	version string
+}
+type spanishBot struct {
+	version string
+}
 
 func main() {
-	eb := englishBot{}
-	sb := spanishBot{}
+	eb := englishBot{version: "1.3.5"}
+	sb := spanishBot{version: "1.2.0"}
 	printGreeting(eb)
+	printVersion(eb)
 	printGreeting(sb)
+	printVersion(sb)
 
 }
 
@@ -31,6 +38,20 @@ func (sb spanishBot) getGreeting(name string) string {
 	return "Hola " + name + ", como estas?"
 }
 
+func (eb englishBot) getVersion() string {
+	// custom logic to get english greeting
+	return eb.version
+}
+
+func (sb spanishBot) getVersion() string {
+	// custom logic to get spanish greeting
+	return sb.version
+}
+
 func printGreeting(tb templateBot) {
-	fmt.Println(tb.getGreeting("Mike"))
+	fmt.Println("bot version", tb.getGreeting("Mike"))
+}
+
+func printVersion(tb templateBot) {
+	fmt.Println("bot version", tb.getVersion())
 }
