4100861596e532f1ac7a88e61de33814eef809d5
Eduardo Margulis <edumarg@gmx.com>
2022-06-23 15:44:09 +0300

    added channel

--- channels/main.go
+++ channels/main.go
@@ -10,18 +10,24 @@ func main() {
 		"http://www.google.com",
 		"http://www.facebook.com",
 		"http://www.amazon.com",
-		"http://www.stackoverflow.com"}
+		"http://www.stackoverflow.com",
+	}
+
+	c := make(chan string) // creating a channel
 
 	for _, URL := range URLs {
-		checkURL(URL)
+		go checkURL(URL, c) //go routine. go keyword use only in front of functions calls.
+		fmt.Println(<-c)
 	}
 }
 
-func checkURL(URL string) {
+func checkURL(URL string, c chan string) {
 	_, err := http.Get(URL)
 	if err != nil {
-		fmt.Println(URL, "might be down!")
+		// fmt.Println(URL, "might be down!")
+		c <- "Sorry " + URL + " might be down!"
 		return
 	}
-	fmt.Println(URL, "is up!")
+	// fmt.Println(URL, "is up!")
+	c <- "Yes " + URL + " is up!"
 }
